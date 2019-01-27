c2320f70e96dbdb1fa30dc81f79590760842588d
005029 <9385935a>
2019-01-27 12:21:58 +0700

    add chap 4

--- js/chapter.js
+++ js/chapter.js
@@ -37,6 +37,18 @@ var books = [
     },
     {
         price: 'Free',
+        name: 'ฉันนี่แหละคือซูเปอร์สตาร์ ภาค 4',
+        type: 'แฟนตาซี',
+        images: 'd1.jpg',
+        folder: 'd/4/',
+        chapters: 100,
+        status: 'Complete',
+        by: 'ฉางอวี้ (尝谕)',
+        // banner: 'new',
+        main: true
+    },
+    {
+        price: 'Free',
         name: 'ผนึกสวรรค์ สยบมาร สะท้านเทพ ภาค 1 - ปรมาจารย์เอกะเทวะ',
         type: 'กำลังภายใน',
         images: 'c1.jpg',
